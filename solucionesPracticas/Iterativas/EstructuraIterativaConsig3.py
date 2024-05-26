b192fd902c470c0d88b83fe99551f517f52e8096
Cecilinares <mcecilialinares@gmail.com>
2024-05-26 20:37:05 -0300

    Add files via upload

---
+++ solucionesPracticas/Iterativas/EstructuraIterativaConsig3.py
@@ -0,0 +1,7 @@
+# Pedir que el usuario ingrese (input) nombre de personas y mostrarlos hasta que el valor de lo que ingresa sea “fin”
+
+nombre = input("Ingrese un nombre: ")
+
+while nombre != "fin" and nombre != "FIN" and nombre != "Fin":
+    print(f"El nombre ingresado es: {nombre}.")
+    nombre = input("Ingrese un nombre: ")
---
+++ solucionesPracticas/Iterativas/EstructurasIterativasConsig1.py
@@ -0,0 +1,7 @@
+# Mostrar los números desde el 0 al número solicitado al usuario
+
+numero = int(input("Ingrese un número: "))
+
+for i in range(int(numero) + 1):
+
+    print (i)
